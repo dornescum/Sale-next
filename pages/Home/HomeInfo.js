a3336bc139575b8cfe6c9a517f2853bd08e69897
Mihai Dornescu <dornescu@gmail.com>
2021-06-25 20:00:53 +0300

    home info

--- pages/Home/HomeInfo.js
+++ pages/Home/HomeInfo.js
@@ -15,39 +15,42 @@ const HomeInfo = (props) => {
     return <Jumbotron fluid className={classes.info}>
         <Container>
             <Row>
-                <Col lg="4" md="6" sm="3" xs="4">
+                <Col lg="4" md="6"  xs="4" >
                     <div className={classes.puppy}>
                         {/*Fixme nu merge hover*/}
-                        <OverlayTrigger overlay={<Tooltip id="tooltip">Eu sunt Eva!</Tooltip>}>
+                        {/*<OverlayTrigger overlay={<Tooltip id="tooltip">Eu sunt Eva!</Tooltip>}>*/}
                             <Image
                                 src={Eva} alt={eva}
                                 />
-                        </OverlayTrigger>
+                        {/*</OverlayTrigger>*/}
                     </div>
                 </Col>
-                <Col lg="8" md="6" sm="9" xs="8">
-                    <h1 className="home-title">Apartamentul de langa nuci</h1>
-                    <div className="home-presentation">
-                        <p id="para">
-                            Are 2 camere si este situat intr un bloc construit in 2011, situat intr o zonă linistită din
-                            Sectorul 3, cu multiple puncte de interes precum scoli, gradinite, supermarketuri si
-                            mijloace de transport in comun in apropire. Stația de metrou 1 Decembrie este la distanta de
-                            maxim 10 min de mers pe jos, 900 . Apartamentul este amenajat modern, complet echipat,
-                            inclusive centrala termica proprie, fiind contorizat independent la toate utilitatile.
+                <Col lg="8" md="6" xs='8'>
 
-                        </p>
-                    </div>
+                        <h1 className={classes.hometitle}>Apartamentul de langa nuci</h1>
+                        <div className={classes.presentation}>
+                            <p id="para">
+                                Are 2 camere si este situat intr un bloc construit in 2011, situat intr o zonă linistită din
+                                Sectorul 3, cu multiple puncte de interes precum scoli, gradinite, supermarketuri si
+                                mijloace de transport in comun in apropire. Stația de metrou 1 Decembrie este la distanta de
+                                maxim 10 min de mers pe jos, 900 . Apartamentul este amenajat modern, complet echipat,
+                                inclusive centrala termica proprie, fiind contorizat independent la toate utilitatile.
+
+                            </p>
+                        </div>
+
+
                 </Col>
             </Row>
-            {/*<div className="home-presentation">*/}
-            {/*    <p id="para-mobile">*/}
-            {/*        Are 2 camere si este situat intr un bloc construit in 2011, situat intr o zonă linistită din*/}
-            {/*        Sectorul 3, cu multiple puncte de interes precum scoli, gradinite, supermarketuri si mijloace de*/}
-            {/*        transport in comun in apropire, Statia de metrou 1 Decembrie este la distanta de maxim 10 min de*/}
-            {/*        mers pe jos, 900 m. Apartamentul este amenajat modern, complet echipat, inclusive centrala termica*/}
-            {/*        proprie, fiind contorizat independent la toate utilitatile.*/}
-            {/*    </p>*/}
-            {/*</div>*/}
+            <div>
+                <p className={classes.mobilepresentation}>
+                    Are 2 camere si este situat intr un bloc construit in 2011, situat intr o zonă linistită din
+                    Sectorul 3, cu multiple puncte de interes precum scoli, gradinite, supermarketuri si mijloace de
+                    transport in comun in apropire, Statia de metrou 1 Decembrie este la distanta de maxim 10 min de
+                    mers pe jos, 900 m. Apartamentul este amenajat modern, complet echipat, inclusive centrala termica
+                    proprie, fiind contorizat independent la toate utilitatile.
+                </p>
+            </div>
 
         </Container>
     </Jumbotron>
